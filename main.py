55e8ed1004ce58abe27e69646877e8b6b3caa3ee
behdad mansouri <behdadmansouri@aut.ac.ir>
2021-11-28 18:05:21 +0330

    Scans ports in an IP range for product and version Except it finds nothing

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 import nmap
 
 
-def scan_ip(ip, args):
-    nmy = nmap.PortScannerYield()
-    result = ""
-    # print("scanning in progress...\n")
-    for progressive_result in nmy.scan(hosts=ip + '/24', arguments=args):
-        if int(progressive_result[1]["nmap"]["scanstats"]["uphosts"]):
-            result += progressive_result[0]
-    return result
-
-
-def scan_port(target, start, end):
+def print_scan_internet(iprange, portrange):
     nm = nmap.PortScanner()
     result = ""
-    for i in range(start, end + 1):
-        res = nm.scan(target, str(i))
-        res = res['scan'][target]['tcp'][i]['state']
-        result += f'\nport {i} is {res}.\np'
+    nm.scan(iprange, portrange, '-v -sS')
+    for ip in nm.all_hosts():
+        try:
+            for port in nm[ip]['tcp'].keys():
+                str = f"Port Open:----> {port} -- {nm[ip]['tcp'][port]['product']} -- {nm[ip]['tcp'][port]['version']}"
+                print(str)
+                result += str + "\n"
+        except:
+            pass
     return result
 
 
 if __name__ == '__main__':
-    iprange = "89.43.3.0"
-    iprange2 = "89.43.4.0"
-    start = 0
-    end = 300
-    args = "-sn"
-    result_ipscan = scan_ip(iprange, args)
-    result_portscan = ""
-    for ip in result_ipscan:
-        result_portscan += scan_port(ip, start, end)
+    iprange = "89.43.3.0/24"
+    iprange2 = "89.43.4.0/24"
+    portrange = "1-300"
 
+    testrange = "127.0.0.1/27"
+
+    result_portscan = print_scan_internet(testrange, portrange)
     f = open("result_hostservicefinder.txt", "w")
     f.write(result_portscan)
     f.close()
